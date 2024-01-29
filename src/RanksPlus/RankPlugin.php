f04b1d939597bf3f5c1c813b7e894f09371ba0b9
BajanVlogs <kjolly544@gmail.com>
2024-01-29 00:53:36 -0500

    Update RankPlugin.php
    Added new /setrank (rank) (player)
    also added default rank on first time joining the server as before you'd spawn in with no rank

--- src/RanksPlus/RankPlugin.php
+++ src/RanksPlus/RankPlugin.php
@@ -2,8 +2,11 @@
 
 namespace RanksPlus;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
@@ -21,12 +24,18 @@ class RankPlugin extends PluginBase implements Listener {
         $this->permissions = new Config($this->getDataFolder() . "permissions.yml", Config::YAML);
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("RankPlugin has been enabled!");
+        $this->getLogger()->info("RankPlus has been enabled!");
     }
 
     public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         $name = $player->getName();
+
+        // Check if the player already has a rank
+        if (!$this->ranks->exists(strtolower($name))) {
+            // Player doesn't have a rank, set default rank
+            $this->setPlayerRank($name, 'Member');
+        }
 
         // Get player's rank
         $rank = $this->getPlayerRank($name);
@@ -56,5 +65,39 @@ class RankPlugin extends PluginBase implements Listener {
             'suffix' => $playerRank['suffix'],
             'permissions' => $mergedPermissions,
         ];
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "setrank" && $sender->hasPermission("ranksplus.setrank")) {
+            if (count($args) === 2) {
+                $rankName = $args[0];
+                $playerName = $args[1];
+
+                $this->setPlayerRank($playerName, $rankName);
+                $sender->sendMessage("Set rank of $playerName to $rankName.");
+                return true;
+            } else {
+                $sender->sendMessage("Usage: /setrank <rank> <player>");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private function setPlayerRank($playerName, $rankName) {
+        // Set the rank for the player in the ranks.yml file
+        $this->ranks->set(strtolower($playerName), [
+            'prefix' => TextFormat::WHITE, // You can customize the default prefix
+            'suffix' => TextFormat::RESET, // You can customize the default suffix
+            'permissions' => $this->getRankPermissions($rankName),
+        ]);
+        $this->ranks->save();
+    }
+
+    private function getRankPermissions($rankName) {
+        // Get the permissions associated with the specified rank
+        $rankPermissions = $this->ranks->get($rankName, []);
+
+        return $rankPermissions['permissions'] ?? [];
     }
 }
